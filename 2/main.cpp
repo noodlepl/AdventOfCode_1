100617471b88be5bb2198ab306d686903e5c7674
Adam Kępiński <Adam.Kepinski@tomtom.com>
2020-01-04 18:14:47 +0100

    Load input in 2

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+std::vector<int> loadInput(const std::string& path) {
+  std::ifstream fs(path);
+  std::vector<int> result;
+
+  if (fs.is_open()) {
+    while (fs.good()) {
+      std::string numAsString;
+      std::getline(fs, numAsString, ',');
+      const int num = std::stoi(numAsString);
+      std::cout << num << std::endl;
+      result.push_back(num);
+    }
+  }
+
+  fs.close();
+  
+  return result;
+}
 
 int main() {
-
+  loadInput("input");
   return 0;
 }
