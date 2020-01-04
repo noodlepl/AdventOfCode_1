ad1ecbf501295844248e0a43cf1d16ce18214a9c
Adam Kępiński <Adam.Kepinski@tomtom.com>
2020-01-04 18:56:21 +0100

    AOC2 part 1 complete

--- 2/main.cpp
+++ 2/main.cpp
@@ -12,17 +12,60 @@ std::vector<int> loadInput(const std::string& path) {
       std::string numAsString;
       std::getline(fs, numAsString, ',');
       const int num = std::stoi(numAsString);
-      std::cout << num << std::endl;
+      std::cout << num << ",";
       result.push_back(num);
     }
+    std::cout << std::endl;
   }
 
   fs.close();
-  
+
   return result;
 }
 
 int main() {
-  loadInput("input");
+  auto data = loadInput("input");
+
+  data[1] = 12;
+  data[2] = 2;
+
+  int pos = 0;
+  bool run = true;
+
+  while (run) {
+    auto opcode = data[pos];
+    auto numPos1 = 0;
+    auto numPos2 = 0;
+    auto resultPos = 0;
+
+    switch(opcode) {
+    case 1:
+      numPos1 = data[pos + 1];
+      numPos2 = data[pos + 2];
+      resultPos = data[pos + 3];
+      data[resultPos] = data[numPos1] + data[numPos2];
+      std::cout << "Opcode 1: num[" << numPos1 << "](" << data[numPos1] << ") + num[" << numPos2 << "](" << data[numPos2] << ") = " << data[resultPos] << " at " << resultPos << std::endl;
+      break;
+    case 2:
+      numPos1 = data[pos + 1];
+      numPos2 = data[pos + 2];
+      resultPos = data[pos + 3];
+      data[resultPos] = data[numPos1] * data[numPos2];
+      std::cout << "Opcode 2: num[" << numPos1 << "](" << data[numPos1] << ") * num[" << numPos2 << "](" << data[numPos2] << ") = " << data[resultPos] << " at " << resultPos << std::endl;
+      break;
+    case 99:
+      run = false;
+      break;
+    default:
+      std::cout << "Error opcode " << opcode << std::endl;
+    }
+
+    pos += 4;
+  }
+
+  for (auto num : data) {
+    std::cout << num << ",";
+  }
+
   return 0;
 }
