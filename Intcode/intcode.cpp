8b2160a46c0c930512e0899a2e6ce7d71aa523c4
Adam Kępiński <Adam.Kepinski@tomtom.com>
2020-01-04 19:00:44 +0100

    Separate Intcode

---
+++ Intcode/intcode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Kępiński on 2020-01-04.
+//
+
+#include "intcode.hpp"
---
+++ Intcode/intcode.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Adam Kępiński on 2020-01-04.
+//
+
+#ifndef ADVENTOFCODE_INTCODE_HPP
+#define ADVENTOFCODE_INTCODE_HPP
+
+
+class Intcode {
+
+};
+
+
+
+#endif //ADVENTOFCODE_INTCODE_HPP
