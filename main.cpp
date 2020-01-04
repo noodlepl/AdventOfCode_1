27e22e298a4155154f3d49ccbc6bb6e4211c47bf
Adam Kępiński <Adam.Kepinski@tomtom.com>
2020-01-04 10:58:46 +0100

    Part 2 solved

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 #include <string>
 
 
+int fuelNeededPartOne(int mass) {
+  return mass / 3 - 2;
+}
+
+int fuelNeededPartTwo(int mass) {
+  int fuel = std::max(0, mass / 3 - 2);
+
+  if (fuel > 0) {
+    fuel += fuelNeededPartTwo(fuel);
+  }
+
+  return fuel;
+}
+
 int main() {
   std::ifstream input;
   input.open("input");
@@ -12,7 +26,7 @@ int main() {
     while (std::getline(input, line)) {
       const int lineAsNum = std::stoi(line);
       std::cout << "Read input: " << lineAsNum << std::endl;
-      accum += lineAsNum / 3 - 2;
+      accum += fuelNeededPartTwo(lineAsNum);
       std::cout << "Accumulated fuel: " << accum << std::endl;
     }
   } else {
