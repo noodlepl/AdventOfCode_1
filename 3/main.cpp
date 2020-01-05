cd17e2c1154fbe798d30dd208d11e239c32f52e6
Adam Kępiński <Adam.Kepinski@tomtom.com>
2020-01-05 12:55:41 +0100

    Add AOC3

---
+++ 3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<int> loadInput(const std::string& path) {
+  std::ifstream fs(path);
+  std::vector<int> result;
+
+  if (fs.is_open()) {
+    while (fs.good()) {
+      std::string numAsString;
+      std::getline(fs, numAsString, ',');
+      const int num = std::stoi(numAsString);
+      std::cout << num << ",";
+      result.push_back(num);
+    }
+    std::cout << std::endl;
+  }
+
+  fs.close();
+
+  return result;
+}
+
+int main() {
+
+}
